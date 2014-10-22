df77df9be76ab8d30e9e713b167c050c44d67236
joshua inocencio <snotbot1234@gmai.com>
2014-10-22 13:45:58 -0700

    no message

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -39,7 +39,7 @@ function gameInitialize() {
     canvas.width = screenWidth;
     canvas.height = screenHeight;
     
-   /* document.addEventListener();*/
+    document.addEventListener("keydown", keyboardHandler);
 }
 
 function gameLoop() {
@@ -84,13 +84,13 @@ function snakeUpdate(){
     var snakeHeadX = snake[0].x;
     var snakeHeadY = snake[0].y;
     
-    if(snakeDirection === "down"){
+    if(snakeDirection == "down"){
         snakeHeadY++;
     }
-    else{
+    else if(snakeDirection == "right"){
        snakeHeadX++;
     }
-    
+  
     var snakeTail = snake.pop();
     snakeTail.x = snakeHeadX;
     snakeTail.y = snakeHeadY;
@@ -112,13 +112,27 @@ function foodInitialize(){
 
 function foodDraw(){
     context.fillStyle = "white";
-    context.fillRect(food.x, food.y, snakeSize, snakeSize);
+    context.fillRect(food.x * snakeSize, food.y * snakeSize, snakeSize, snakeSize);
 }
 
 function setFoodPosition(){
     var randomX = Math.floor(Math.random() * screenWidth);
     var randomY = Math.floor(Math.random() * screenHeight);
 
-    food.x = randomX;
-    food.y = randomY;
+    food.x = Math.floor(randomX / snakeSize);
+    food.y = Math.floor(randomY / snakeSize);
+}
+/*-------------------------------------------------------
+ * Input Functions
+ * ------------------------------------------------------
+ */
+function keyboardHandler(event){
+    console.log(event);
+    
+    if(event.keyCode === "39" && snakeDirection !== "left"){
+        snakeDirection = "right";
+    }
+    else if (event.keyCode === "40" && snakeDirection !== "up"){
+        snakeDirection = "down";
+    }
 }
